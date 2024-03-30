e54e229fe7e942797fe51da67a826bc9d0994266
azatbek <sadykowazatbek@gmail.com>
2024-03-30 13:05:16 +0600

    Добавлена возможность регистрации, логина и логина через google аккаунт.

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ import cors from 'cors';
 import express from 'express';
 import mongoose from "mongoose";
 import config from "./config";
+import usersRouter from "./routers/users";
 
 const app = express();
 const port = 8000;
@@ -10,6 +11,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
 
+app.use('/users', usersRouter);
 
 const run = async () => {
   await  mongoose.connect(config.mongoose.db)
