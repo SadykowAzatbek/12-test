ad6c688b1bfae10719da1e6258b65cdeef5a54bb
azatbek <sadykowazatbek@gmail.com>
2024-03-30 15:24:04 +0600

    Добавлен AppToolbar, Login, Register, теперь можно регистрироваться, логиниться и входить через google аккаунт. Добавлен axiosApi для выполнение запросов.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,22 @@
+import {Route, Routes } from 'react-router-dom';
 import './App.css';
+import AppToolbar from './components/UI/AppToolbar.tsx';
+import Register from './features/Users/Register.tsx';
+import Login from './features/Users/Login.tsx';
+import {Alert} from '@mui/material';
 
 function App() {
 
   return (
     <>
+      <header>
+        <AppToolbar />
+      </header>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Alert severity="error">Страница не найдена!</Alert>} />
+      </Routes>
     </>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {persistor, store} from './App/store.ts';
+import {PersistGate} from 'redux-persist/integration/react';
+import {addInterceptors} from './axiosApi.ts';
+import {GoogleOAuthProvider} from '@react-oauth/google';
+import {GOOGLE_CLIENT_ID} from './constants.ts';
+
+addInterceptors(store);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <Provider store={store}>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </GoogleOAuthProvider>
+  </Provider>
+);
