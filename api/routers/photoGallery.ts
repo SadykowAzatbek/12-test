c6f178973be6b037e6095278c599d5c066b511f0
azatbek <sadykowazatbek@gmail.com>
2024-03-30 14:52:51 +0600

    Теперь админы могут удалять любые фото, по запросу DELETE /photos/:id. Так же, пользователи могут удалять свои фото DELETE /photos?photo=photoId.

--- api/routers/photoGallery.ts
+++ api/routers/photoGallery.ts
@@ -3,6 +3,7 @@ import PhotoGallery from "../models/PhotoGallery";
 import auth, {RequestWithUser} from "../middleware/auth";
 import {imageUpload} from "../multer";
 import {Types} from "mongoose";
+import permit from "../middleware/permit";
 
 const photoGalleryRouter = Router();
 
@@ -10,7 +11,7 @@ photoGalleryRouter.get('/', async (_req, res, next) => {
   try {
     const photos = await PhotoGallery.find();
 
-    res.send(photos);
+    return res.send(photos);
   } catch (err) {
     return next(err);
   }
@@ -18,25 +19,21 @@ photoGalleryRouter.get('/', async (_req, res, next) => {
 
 photoGalleryRouter.get('/:id', async (req, res, next) => {
   try {
+    let _id: Types.ObjectId;
+
     try {
-      let _id: Types.ObjectId;
-
-      try {
-        _id = new Types.ObjectId(req.params.id);
-      } catch {
-        return res.status(422).send({error: 'Wrong objectId!'});
-      }
-
-      const photos = await PhotoGallery.find({user: _id});
-
-      if (!photos) {
-        return res.status(422).send({error: 'Not found!'});
-      }
-
-      res.send(photos);
-    } catch (err) {
-      next(err);
+      _id = new Types.ObjectId(req.params.id);
+    } catch {
+      return res.status(422).send({error: 'Wrong objectId!'});
     }
+
+    const photos = await PhotoGallery.find({user: _id});
+
+    if (!photos) {
+      return res.status(422).send({error: 'Not found!'});
+    }
+
+    return res.send(photos);
   } catch (err) {
     return next(err);
   }
@@ -52,14 +49,51 @@ photoGalleryRouter.post('/', auth, imageUpload.single('image'), async (req: Requ
 
     await photo.save();
 
-    res.send(photo);
+    return res.send(photo);
   } catch (err) {
     return next(err);
   }
 });
 
-photoGalleryRouter.get('/:id', async (req, res, next) => {
+photoGalleryRouter.delete('/:id', auth, permit('admin'), async (req, res, next) => {
   try {
+    let _id: Types.ObjectId;
+
+    try {
+      _id = new Types.ObjectId(req.params.id);
+    } catch {
+      return res.status(422).send({error: 'Wrong objectId!'});
+    }
+
+    const deleteOnePhoto = await PhotoGallery.findByIdAndDelete(_id);
+
+    if (!deleteOnePhoto) {
+      return res.status(422).send({error: 'Not found!'});
+    }
+
+    return res.send({message: 'One photo deleted'});
+  } catch (err) {
+    return next(err);
+  }
+});
+
+photoGalleryRouter.delete('/', auth, async (req: RequestWithUser, res, next) => {
+  try {
+    const photoId = req.query.photo as string;
+
+    const photo = await PhotoGallery.findById(photoId);
+
+    if (!photo) {
+      return res.status(404).send({error: 'Photo not found'});
+    }
+
+    if (photo.user.toString() !== req.user?._id.toString()) {
+      return res.status(403).send({error: 'Access denied!!'});
+    }
+
+    await PhotoGallery.findByIdAndDelete(photoId);
+
+    return res.send({message: 'One photo deleted'});
 
   } catch (err) {
     return next(err);
